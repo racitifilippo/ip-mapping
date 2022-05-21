4dcc2901af4c070f983029283efde63821e3be33
racitifilippo <raciti.filippo@iisgalvanimi.edu.it>
2022-05-21 23:50:18 +0200

    update input

--- ipMapping.py
+++ ipMapping.py
@@ -3,14 +3,26 @@ from threading import Thread
 import subprocess
 
 ind = []
-net='192.168.40.'
+net = ''
+while 1:
+    try:
+        net= input('Scrivere indirizzo ipv4 senza lultimo ottetto valido: ')
+    except:
+        1
+    n = 0
+    for i in range(len(net)):
+        if net[i] == '.':
+            n += 1
+    if n == 3 :
+        break
+
 volte = 255
 
 
 def ping(host):
     print('++++ start ' + net + host)
     #x = subprocess(['ping', net + host])
-    x = os.popen('ping ' + net + host).read().split('\n')[2]
+    x = os.popen('ping -c 3 ' + net + host).read().split('\n')[2]
     print(x)
     if 'byte' in x:
         ind.append(net + host)
